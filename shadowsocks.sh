2a104fd00c2ae15641eed9ce268d21d3c4acc2d2
tg10020617 <1639619652@qq.com>
2020-03-23 22:17:49 +0800

    Update shadowsocks.sh

--- shadowsocks.sh
+++ shadowsocks.sh
@@ -14,28 +14,28 @@ pip install shadowsocks
 # 写入配置文件
 touch /etc/shadowsocks.json
 
-#echo -e "{" >> /etc/shadowsocks.json
-#echo -e "\"server\": \"0.0.0.0\"," >> /etc/shadowsocks.json
-#echo -e "\"server_port\": 8388," >> /etc/shadowsocks.json
-#echo -e "\"password\": \"Q5konCq7rfUQ1KAd\"," >> /etc/shadowsocks.json
-#echo -e "\"method\": \"aes-256-cfb\"" >> /etc/shadowsocks.json
-#echo -e "}" >> /etc/shadowsocks.json
+echo -e "{" >> /etc/shadowsocks.json
+echo -e "\"server\": \"0.0.0.0\"," >> /etc/shadowsocks.json
+echo -e "\"server_port\": 8388," >> /etc/shadowsocks.json
+echo -e "\"password\": \"Q5konCq7rfUQ1KAd\"," >> /etc/shadowsocks.json
+echo -e "\"method\": \"aes-256-cfb\"" >> /etc/shadowsocks.json
+echo -e "}" >> /etc/shadowsocks.json
 
-cat > /etc/shadowsocks.json <<-EOF
-{
-    "server":"0.0.0.0",
-    "local_address":"127.0.0.1",
-    "local_port":1080,
-    "port_password": {
-        "9377":"gRvR04C988VB",
-        "9001":"2MgdE27klL9N",
-        "9820":"wDexVhSTJoEC"
-    },
-    "timeout":300,
-    "method":"aes-256-cfb",
-    "fast_open":false
-}
-EOF
+#cat > /etc/shadowsocks.json <<-EOF
+#{
+#    "server":"0.0.0.0",
+#    "local_address":"127.0.0.1",
+#    "local_port":1080,
+#    "port_password": {
+#        "9377":"gRvR04C988VB",
+#        "9001":"2MgdE27klL9N",
+#        "9820":"wDexVhSTJoEC"
+#    },
+#    "timeout":300,
+#    "method":"aes-256-cfb",
+#    "fast_open":false
+#}
+#EOF
 
 # 写入启动脚本文件
 touch /etc/systemd/system/shadowsocks.service
